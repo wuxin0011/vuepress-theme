dbe08715ec2780c0e8fb12afb5b136cae2fb19a0
wuxin001 <2191377759@qq.com>
2023-05-02 16:05:25 +0800

    deploy https://wuxin0011.github.io/vuepress2/

---
+++ assets/deployment.html-4032e156.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-e8a66a70","path":"/guide/deployment.html","title":"部署","lang":"en-US","frontmatter":{},"headers":[{"level":2,"title":"部署方式","slug":"部署方式","link":"#部署方式","children":[]}],"git":{"updatedTime":1683013924000,"contributors":[{"name":"wuxin001","email":"2191377759@qq.com","commits":1}]},"filePathRelative":"guide/deployment.md"}');export{e as data};
---
+++ assets/index.html-c46593ca.js
@@ -0,0 +1,38 @@
+import{_ as t,M as i,p as o,q as d,N as s,V as a,a1 as u,R as n,t as e}from"./framework-5866ffd3.js";const r={},m=u(`<h3 id="像数-1-2-3-一样容易" tabindex="-1"><a class="header-anchor" href="#像数-1-2-3-一样容易" aria-hidden="true">#</a> 像数 1, 2, 3 一样容易</h3><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">git</span> clone https://github.com/wuxin0011/vuepress2
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="运行-打包" tabindex="-1"><a class="header-anchor" href="#运行-打包" aria-hidden="true">#</a> 运行 打包</h2>`,3),p=n("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token comment"},"# 安装依赖"),e(`
+`),n("span",{class:"token function"},"pnpm"),e(),n("span",{class:"token function"},"install"),e(` 
+
+`),n("span",{class:"token comment"},"# 启动"),e(`
+`),n("span",{class:"token function"},"pnpm"),e(` run dev
+
+`),n("span",{class:"token comment"},"# 打包"),e(`
+`),n("span",{class:"token function"},"pnpm"),e(` run build
+
+`),n("span",{class:"token comment"},"# 发布"),e(`
+`),n("span",{class:"token function"},"pnpm"),e(` run deploy
+
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),b=n("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token comment"},"# 安装依赖"),e(`
+`),n("span",{class:"token function"},"yarn"),e(),n("span",{class:"token function"},"install"),e(` 
+
+`),n("span",{class:"token comment"},"# 启动"),e(`
+`),n("span",{class:"token function"},"yarn"),e(` dev
+
+`),n("span",{class:"token comment"},"# 打包"),e(`
+`),n("span",{class:"token function"},"yarn"),e(`  build
+
+`),n("span",{class:"token comment"},"# 发布"),e(`
+`),n("span",{class:"token function"},"yarn"),e(` deploy
+
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),v=n("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token comment"},"# 安装依赖"),e(`
+`),n("span",{class:"token function"},"npm"),e(),n("span",{class:"token function"},"install"),e(` 
+
+`),n("span",{class:"token comment"},"# 启动"),e(`
+`),n("span",{class:"token function"},"npm"),e(` run dev
+
+`),n("span",{class:"token comment"},"# 打包"),e(`
+`),n("span",{class:"token function"},"npm"),e(` run build
+
+`),n("span",{class:"token comment"},"# 发布"),e(`
+`),n("span",{class:"token function"},"npm"),e(` run deploy
+
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1);function h(k,f){const l=i("CodeGroupItem"),c=i("CodeGroup");return o(),d("div",null,[m,s(c,null,{default:a(()=>[s(l,{title:"PNPM",active:""},{default:a(()=>[p]),_:1}),s(l,{title:"YARN"},{default:a(()=>[b]),_:1}),s(l,{title:"NPM"},{default:a(()=>[v]),_:1})]),_:1})])}const g=t(r,[["render",h],["__file","index.html.vue"]]);export{g as default};
