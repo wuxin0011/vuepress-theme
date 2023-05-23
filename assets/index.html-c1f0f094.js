ec2cd12c9524b699e41e27e292ed9af8689f7d00
wuxin0011 <65836396+wuxin0011@users.noreply.github.com>
2023-05-23 23:14:28 +0800

    Deploying to gh-pages from  @ b1e05eae5ff802d192da5d709d4d97a36b097c14 🚀

---
+++ assets/index.html-c1f0f094.js
@@ -0,0 +1,38 @@
+import{T as o}from"./app-b627a1cd.js";import{_ as d,M as i,p as u,q as r,N as s,V as a,a1 as m,R as n,t as e}from"./framework-bcd7c3e6.js";const p={components:{ToggleColorModeButton:o}},b=m(`<h3 id="像数-1-2-3-一样容易" tabindex="-1"><a class="header-anchor" href="#像数-1-2-3-一样容易" aria-hidden="true">#</a> 像数 1, 2, 3 一样容易</h3><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">git</span> clone https://github.com/wuxin0011/vuepress2
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div>`,2),v=n("h2",{id:"运行-打包",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#运行-打包","aria-hidden":"true"},"#"),e(" 运行 打包")],-1),h=n("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token comment"},"# 安装依赖"),e(`
+`),n("span",{class:"token function"},"pnpm"),e(),n("span",{class:"token function"},"install"),e(` 
+
+`),n("span",{class:"token comment"},"# 启动"),e(`
+`),n("span",{class:"token function"},"pnpm"),e(` run dev
+
+`),n("span",{class:"token comment"},"# 打包"),e(`
+`),n("span",{class:"token function"},"pnpm"),e(` run build
+
+`),n("span",{class:"token comment"},"# 发布"),e(`
+`),n("span",{class:"token function"},"pnpm"),e(` run deploy
+
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),_=n("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token comment"},"# 安装依赖"),e(`
+`),n("span",{class:"token function"},"yarn"),e(),n("span",{class:"token function"},"install"),e(` 
+
+`),n("span",{class:"token comment"},"# 启动"),e(`
+`),n("span",{class:"token function"},"yarn"),e(` dev
+
+`),n("span",{class:"token comment"},"# 打包"),e(`
+`),n("span",{class:"token function"},"yarn"),e(`  build
+
+`),n("span",{class:"token comment"},"# 发布"),e(`
+`),n("span",{class:"token function"},"yarn"),e(` deploy
+
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),f=n("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token comment"},"# 安装依赖"),e(`
+`),n("span",{class:"token function"},"npm"),e(),n("span",{class:"token function"},"install"),e(` 
+
+`),n("span",{class:"token comment"},"# 启动"),e(`
+`),n("span",{class:"token function"},"npm"),e(` run dev
+
+`),n("span",{class:"token comment"},"# 打包"),e(`
+`),n("span",{class:"token function"},"npm"),e(` run build
+
+`),n("span",{class:"token comment"},"# 发布"),e(`
+`),n("span",{class:"token function"},"npm"),e(` run deploy
+
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1);function k(g,x,C,N,y,B){const c=i("ToggleColorModeButton"),l=i("CodeGroupItem"),t=i("CodeGroup");return u(),r("div",null,[b,s(c),v,s(t,null,{default:a(()=>[s(l,{title:"PNPM",active:""},{default:a(()=>[h]),_:1}),s(l,{title:"YARN"},{default:a(()=>[_]),_:1}),s(l,{title:"NPM"},{default:a(()=>[f]),_:1})]),_:1})])}const V=d(p,[["render",k],["__file","index.html.vue"]]);export{V as default};
